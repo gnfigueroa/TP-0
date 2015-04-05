40249d003a26afe7aad7f5a4aafb2d7d82d7b12e
gnfigueroa <gabrieln.figueroa@yahoo.com>
2015-04-05 14:35:00 -0300

    Codigo Fuente Hello World

---
+++ maintp0.cpp
@@ -0,0 +1,15 @@
+#include <cstdlib>
+#include <iostream>
+
+/* Hello World
+* Gabriel Nicolas Figueroa
+* 3 de abril de 2015
+*/
+using namespace std;
+
+int main(void)
+{
+    cout << "Hello World!! \n";
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
